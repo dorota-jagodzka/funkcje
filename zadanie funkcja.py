bd385a9e6bcc5a74ec490b09958596757169d318
Dorota Jagodzka <dorota.jagodzka01@gmail.com>
2021-11-14 14:13:58 +0100

    poprawka

--- zadanie funkcja.py
+++ zadanie funkcja.py
@@ -1,13 +1,13 @@
 def check_if_palindrome(word):
-    word_from_backwords = word[::-1]
-    decision=True
-    if(word != word_from_backwords):
-        decision=False
+    word_spelled_backwards = word[::-1]
+    decision = True
+    if word != word_spelled_backwards:
+        decision = False
     return decision
 
 word = "potop"
-result= check_if_palindrome(word)
-if(result):
+result = check_if_palindrome(word)
+if result :
     print("To słowo jest palindromem")
 else:
     print("To słowo nie jest palindromem")
