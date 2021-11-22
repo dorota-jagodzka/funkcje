9198193df4cf3f3741f2fde0823635f58f049924
Dorota Jagodzka <dorota.jagodzka01@gmail.com>
2021-11-22 17:52:31 +0100

    function body fix

--- zadanie funkcja.py
+++ zadanie funkcja.py
@@ -1,13 +1,9 @@
 def check_if_palindrome(word):
-    word_spelled_backwards = word[::-1]
-    decision = True
-    if word != word_spelled_backwards:
-        decision = False
-    return decision
+    return word == word[::1]
 
 word = "potop"
 result = check_if_palindrome(word)
-if result :
+if result:
     print("To słowo jest palindromem")
 else:
     print("To słowo nie jest palindromem")
